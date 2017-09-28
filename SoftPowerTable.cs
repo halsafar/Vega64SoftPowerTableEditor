c21908b5dfc05422de4806a873cb817761bcc90f
Stephen Damm <shinhalsafar@gmail.com>
2017-09-27 21:56:18 -0600

    - parse gfx clocks - parse mem clocks - parse gfx vdd - parse mem vdd - parse fan table

--- SoftPowerTable.cs
+++ SoftPowerTable.cs
@@ -47,15 +47,15 @@ namespace Vega64SoftPowerTableEditor
 			UCHAR ucVceVoltageMode;
 			UCHAR ucMp0VoltageMode;
 			UCHAR ucDcefVoltageMode;
-			USHORT usStateArrayOffset;                 /* points to ATOM_Vega10_State_Array */
-			USHORT usFanTableOffset;                   /* points to ATOM_Vega10_Fan_Table */
+			public USHORT usStateArrayOffset;                 /* points to ATOM_Vega10_State_Array */
+			public USHORT usFanTableOffset;                   /* points to ATOM_Vega10_Fan_Table */
 			USHORT usThermalControllerOffset;          /* points to ATOM_Vega10_Thermal_Controller */
 			USHORT usSocclkDependencyTableOffset;      /* points to ATOM_Vega10_SOCCLK_Dependency_Table */
-			USHORT usMclkDependencyTableOffset;        /* points to ATOM_Vega10_MCLK_Dependency_Table */
-			USHORT usGfxclkDependencyTableOffset;      /* points to ATOM_Vega10_GFXCLK_Dependency_Table */
+			public USHORT usMclkDependencyTableOffset;        /* points to ATOM_Vega10_MCLK_Dependency_Table */
+			public USHORT usGfxclkDependencyTableOffset;      /* points to ATOM_Vega10_GFXCLK_Dependency_Table */
 			USHORT usDcefclkDependencyTableOffset;     /* points to ATOM_Vega10_DCEFCLK_Dependency_Table */
-			USHORT usVddcLookupTableOffset;            /* points to ATOM_Vega10_Voltage_Lookup_Table */
-			USHORT usVddmemLookupTableOffset;          /* points to ATOM_Vega10_Voltage_Lookup_Table */
+			public USHORT usVddcLookupTableOffset;            /* points to ATOM_Vega10_Voltage_Lookup_Table */
+			public USHORT usVddmemLookupTableOffset;          /* points to ATOM_Vega10_Voltage_Lookup_Table */
 			USHORT usMMDependencyTableOffset;          /* points to ATOM_Vega10_MM_Dependency_Table */
 			USHORT usVCEStateTableOffset;              /* points to ATOM_Vega10_VCE_State_Table */
 			USHORT usReserve;                          /* No PPM Support for Vega10 */
@@ -68,6 +68,116 @@ namespace Vega64SoftPowerTableEditor
 			USHORT usPhyClkDependencyTableOffset;      /* points to ATOM_Vega10_PHYCLK_Dependency_Table */
 		};
 
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_State
+		{
+			UCHAR ucSocClockIndexHigh;
+			UCHAR ucSocClockIndexLow;
+			UCHAR ucGfxClockIndexHigh;
+			UCHAR ucGfxClockIndexLow;
+			UCHAR ucMemClockIndexHigh;
+			UCHAR ucMemClockIndexLow;
+			USHORT usClassification;
+			ULONG ulCapsAndSettings;
+			USHORT usClassification2;
+		};
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_State_Array
+		{
+			public UCHAR ucRevId;
+			public UCHAR ucNumEntries;                                         /* Number of entries. */
+		};
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_GFXCLK_Dependency_Table
+		{
+			public UCHAR ucRevId;
+			public UCHAR ucNumEntries;                                         /* Number of entries. */
+		};
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_GFXCLK_Dependency_Record
+		{
+			public ULONG ulClk;                                               /* Clock Frequency */
+			public UCHAR ucVddInd;                                            /* SOC_VDD index */
+			public USHORT usCKSVOffsetandDisable;                              /* Bits 0~30: Voltage offset for CKS, Bit 31: Disable/enable for the GFXCLK level. */
+			public USHORT usAVFSOffset;                                        /* AVFS Voltage offset */
+			public UCHAR ucACGEnable;
+			public fixed UCHAR ucReserved[3];
+		};
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_MCLK_Dependency_Table
+		{
+		public UCHAR ucRevId;
+		public UCHAR ucNumEntries;                                         /* Number of entries. */
+		};
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_MCLK_Dependency_Record
+		{
+		public ULONG ulMemClk;                                            /* Clock Frequency */
+		public UCHAR ucVddInd;                                            /* SOC_VDD index */
+		public UCHAR ucVddMemInd;                                         /* MEM_VDD - only non zero for MCLK record */
+		public UCHAR ucVddciInd;                                          /* VDDCI   = only non zero for MCLK record */
+		};
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_Voltage_Lookup_Record
+		{
+			public USHORT usVdd;                                               /* Base voltage */
+		};
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_Voltage_Lookup_Table
+		{
+			public UCHAR ucRevId;
+			public UCHAR ucNumEntries;                                          /* Number of entries */
+		};
+
+		[StructLayout(LayoutKind.Sequential, Pack = 1)]
+		public unsafe struct ATOM_Vega10_Fan_Table
+		{
+			public UCHAR ucRevId;                         /* Change this if the table format changes or version changes so that the other fields are not the same. */
+			public USHORT usFanOutputSensitivity;          /* Sensitivity of fan reaction to temepature changes. */
+			public USHORT usFanRPMMax;                     /* The default value in RPM. */
+			public USHORT usThrottlingRPM;
+			public USHORT usFanAcousticLimit;              /* Minimum Fan Controller Frequency Acoustic Limit. */
+			public USHORT usTargetTemperature;             /* The default ideal temperature in Celcius. */
+			public USHORT usMinimumPWMLimit;               /* The minimum PWM that the advanced fan controller can set. */
+			public USHORT usTargetGfxClk;                   /* The ideal Fan Controller GFXCLK Frequency Acoustic Limit. */
+			public USHORT usFanGainEdge;
+			public USHORT usFanGainHotspot;
+			public USHORT usFanGainLiquid;
+			public USHORT usFanGainVrVddc;
+			public USHORT usFanGainVrMvdd;
+			public USHORT usFanGainPlx;
+			public USHORT usFanGainHbm;
+			public UCHAR ucEnableZeroRPM;
+			public USHORT usFanStopTemperature;
+			public USHORT usFanStartTemperature;
+		};
+
+
+		public ATOM_POWERPLAY_TABLE atom_powerplay_table;
+
+		public ATOM_Vega10_State_Array atom_vega10_state_array;
+
+		public ATOM_Vega10_GFXCLK_Dependency_Table atom_vega10_gfxclk_table;
+		public List<ATOM_Vega10_GFXCLK_Dependency_Record> atom_vega10_gfxclk_entries = new List<ATOM_Vega10_GFXCLK_Dependency_Record>();
+
+		public ATOM_Vega10_MCLK_Dependency_Table atom_vega10_memclk_table;
+		public List<ATOM_Vega10_MCLK_Dependency_Record> atom_vega10_memclk_entries = new List<ATOM_Vega10_MCLK_Dependency_Record>();
+
+		public ATOM_Vega10_Voltage_Lookup_Table atom_vega10_gfxvdd_table;
+		public List<ATOM_Vega10_Voltage_Lookup_Record> atom_vega10_gfxvdd_record = new List<ATOM_Vega10_Voltage_Lookup_Record>();
+
+		public ATOM_Vega10_Voltage_Lookup_Table atom_vega10_memvdd_table;
+		public List<ATOM_Vega10_Voltage_Lookup_Record> atom_vega10_memvdd_record = new List<ATOM_Vega10_Voltage_Lookup_Record>();
+
+		public ATOM_Vega10_Fan_Table atom_vega10_fan_table;
+
 		private static String STR_WIN_VER = "Windows Registry Editor Version";
 		private Regex REGEX_WIN_VER = new Regex("Windows Registry Editor Version (.*)", RegexOptions.IgnorePatternWhitespace);
 
@@ -76,8 +186,6 @@ namespace Vega64SoftPowerTableEditor
 
 		public String windowsRegistryVersion = null;
 		public List<int> hexBlobNewLineIndices = new List<int>();
-
-		public ATOM_POWERPLAY_TABLE atom_powerplay_table;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="T:Vega64SoftPowerTableEditor.SoftPowerTable"/> class.
@@ -137,8 +245,39 @@ namespace Vega64SoftPowerTableEditor
 
 			byte[] byteArray = StringToByteArray(hexData);
 
-			// parse into structure
+			// parse main table
 			spt.atom_powerplay_table = fromBytes<ATOM_POWERPLAY_TABLE>(byteArray);
+
+			// parse state array
+			int offset = spt.atom_powerplay_table.usStateArrayOffset;
+			spt.atom_vega10_state_array = fromBytes<ATOM_Vega10_State_Array>(byteArray.Skip(offset).ToArray());
+
+			// parse gfx clk states (hurray!)
+			offset = spt.atom_powerplay_table.usGfxclkDependencyTableOffset;
+			spt.atom_vega10_gfxclk_table = fromBytes<ATOM_Vega10_GFXCLK_Dependency_Table>(byteArray.Skip(offset).ToArray());
+			offset += Marshal.SizeOf(spt.atom_vega10_gfxclk_table);
+			spt.atom_vega10_gfxclk_entries = arrFromBytes<ATOM_Vega10_GFXCLK_Dependency_Record>(byteArray, offset, spt.atom_vega10_gfxclk_table.ucNumEntries);
+
+			// parse mem clk states
+			offset = spt.atom_powerplay_table.usMclkDependencyTableOffset;
+			spt.atom_vega10_memclk_table = fromBytes<ATOM_Vega10_MCLK_Dependency_Table>(byteArray.Skip(offset).ToArray());
+			offset += Marshal.SizeOf(spt.atom_vega10_memclk_table);
+			spt.atom_vega10_memclk_entries = arrFromBytes<ATOM_Vega10_MCLK_Dependency_Record>(byteArray, offset, spt.atom_vega10_memclk_table.ucNumEntries);
+
+			// parse gfx voltages
+			offset = spt.atom_powerplay_table.usVddcLookupTableOffset;
+			spt.atom_vega10_gfxvdd_table = fromBytes<ATOM_Vega10_Voltage_Lookup_Table>(byteArray.Skip(offset).ToArray());
+			offset += Marshal.SizeOf(spt.atom_vega10_gfxvdd_table);
+			spt.atom_vega10_gfxvdd_record = arrFromBytes<ATOM_Vega10_Voltage_Lookup_Record>(byteArray, offset, spt.atom_vega10_gfxvdd_table.ucNumEntries);
+
+			// parse mem voltages
+			offset = spt.atom_powerplay_table.usVddmemLookupTableOffset;
+			spt.atom_vega10_memvdd_table = fromBytes<ATOM_Vega10_Voltage_Lookup_Table>(byteArray.Skip(offset).ToArray());
+			offset += Marshal.SizeOf(spt.atom_vega10_gfxvdd_table);
+			spt.atom_vega10_memvdd_record = arrFromBytes<ATOM_Vega10_Voltage_Lookup_Record>(byteArray, offset, spt.atom_vega10_memvdd_table.ucNumEntries);
+
+			// parse fan table
+			spt.atom_vega10_fan_table = fromBytes<ATOM_Vega10_Fan_Table>(byteArray.Skip(spt.atom_powerplay_table.usFanTableOffset).ToArray());
 
 			// debug
 			Console.WriteLine(data);
@@ -163,6 +302,19 @@ namespace Vega64SoftPowerTableEditor
 			Marshal.FreeHGlobal(ptr);
 
 			return obj;
+		}
+
+		static List<T> arrFromBytes<T>(byte[] arr, int offset, int entries)
+		{
+			List<T> retVal = new List<T>();
+			for (int i = 0; i< entries; i++)
+			{				
+				T record = fromBytes<T>(arr.Skip(offset).ToArray());
+				retVal.Add(record);
+				offset += Marshal.SizeOf(record);
+			}
+
+			return retVal;
 		}
 
 		/// <summary>
